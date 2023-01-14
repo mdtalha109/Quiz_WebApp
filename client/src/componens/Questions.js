0210b8ec0d6e03412fe73399006b006da9b91d91
mdtalha109 <mdtalha1041@gmail.com>
2023-01-15 00:21:43 +0530

    created question component

--- client/src/componens/Questions.js
+++ client/src/componens/Questions.js
@@ -1,6 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import data from '../data/data'
+
 const Questions = () => {
+
+    const [checked, setChecked] = useState(undefined)
+
+    const question = data[0]
+
+    useEffect(()=> {
+
+    })
+
+    const onSelect = () => {
+        setChecked(true)
+        alert("radio");
+    }
+
     return(
-        <h1>Questions compoenent</h1>
+        <div className="questions">
+            <h2 className="text-light">{question.question}</h2>
+            <ul key={question.id}>
+               {
+                question.options.map((question, index) => (
+                    <li key={index}>
+                    <input 
+                        type="radio" 
+                        value={false} 
+                        name="options"
+                        id={`q${index}-options`}
+                        onChange={onSelect}
+                        
+                    />
+
+                    <label className='text-primary' htmlFor={`q${index}-options`}> {question} </label>
+                    <div className='check'></div>
+                </li>  
+                ))
+               }              
+            </ul>
+
+        </div>
     )
 }
 
