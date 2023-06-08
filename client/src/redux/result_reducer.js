805f71a475d825a93c48bafce821cb65f93cb0a2
mdtalha-eronkan <md.talha@eronkan.com>
2023-06-08 16:35:00 +0530

    added result reducer

--- client/src/redux/result_reducer.js
+++ client/src/redux/result_reducer.js
@@ -1,3 +1,22 @@
-export const resultReducer = () => {
+import {createSlice} from '@reduxjs/toolkit'
 
-}
+export const resultReducer = createSlice({
+    name : 'result',
+    initialState: {
+        userId: null,
+        result: []
+    },
+
+    reducers: {
+        setUserId: (state, action) => {
+            state.userId = action.payload
+        },
+
+        pushResultAction: (state, action) => {
+            state.result.push(action.payload)
+        }
+    }
+})
+
+export const {setUserId, pushResultAction} = resultReducer.actions
+export default resultReducer.reducer;
