50391e9ef5bcbb760658a21ba4ebf4ca986fcc5b
mdtalha109 <mdtalha1041@gmail.com>
2023-07-08 17:42:58 +0530

    feat: added pushing answer to result

--- client/src/componens/Quiz.js
+++ client/src/componens/Quiz.js
@@ -1,46 +1,85 @@
 import Questions from "./Questions"
 
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { nextQuestion, prevQuestion } from "../redux/question_reducer"
+import { pushAnswer } from "../hooks/setResult"
+import { useNavigate } from 'react-router-dom';
 
-export default function Quiz(){
 
+import Confetti from 'react-confetti'
+
+
+export default function Quiz() {
+
+    const navigate = useNavigate()
+
+    const [checked, setChecked] = useState(undefined)
+
+    // const { width, height } = useWindowSize()
+    const width = window.innerWidth
+    const height = window.innerHeight
     const dispatch = useDispatch()
 
-    const state = useSelector(state => state)
+    const { questions, results } = useSelector(state => state)
+    console.log(questions)
+    // console.log("results: ", results)
 
-    useEffect(()=> {
-        console.log(state)
+    // let trace = questions.trace
+
+
+
+    useEffect(() => {
+
     })
 
     const onNext = () => {
-
-        if(state.questions.queue.length === state.questions.trace + 1){
+        // alert(`questions.trace is ${questions.trace} and `)
+        if (questions.trace === questions.queue.length - 1) {
             alert("No next question");
             return
         }
-        
+
         dispatch(nextQuestion())
+        dispatch(pushAnswer(checked))
     }
 
     const onPrev = () => {
-        if(state.questions.trace < 1){
+        if (questions.trace <= 0) {
             alert("No prev question");
             return
         }
         dispatch(prevQuestion())
     }
 
+    const onChecked = (check) => {
+        setChecked(check)
+        setChecked(undefined)
+    }
+
+    // check if user reached last question, then exam has to be ended and user should be navigated to result page
+    console.log(`questions.queue.length is ${questions.queue.length} and trace is ${questions.trace}`)
+    if(questions && questions.queue.length-1 === questions.trace){
+        alert('inside')
+        navigate('/result')
+    }
+
+
     return (
         <div className="container">
+            {/* <Confetti
+      width={width}
+      height={height}
+      drawShape
+      
+    /> */}
             <h1 className="title text-light">Quiz Application</h1>
 
             {/* Questions component */}
-            <Questions/>
-
+            
+            <Questions onChecked = {onChecked} />
             <div clsssName='grid'>
-                <button className="btn prev" onClick={onPrev}>Prev</button>
+                <button className="btn prev" onClick={onPrev} disabled={questions.trace <= 0}>Prev</button>
                 <button className="btn next" onClick={onNext}>Next</button>
             </div>
         </div>
