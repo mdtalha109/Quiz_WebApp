5a426970b2f5a70d3c1f3f34c12e766999edeee6
mdtalha109 <mdtalha1041@gmail.com>
2023-07-16 00:45:15 +0530

    seeder

---
+++ server/seeder.js
@@ -0,0 +1,8 @@
+const data = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        options: ['<scripting>', '<javascript>', '<js>', '<script>'],
+        correctAnswer: 'd',
+        category: 'Javascript'
+    }
+]
