774c9c1fa29dde48ff89e1dad199f1ca35b08e93
mdtalha109 <mdtalha1041@gmail.com>
2023-07-16 22:14:01 +0530

    feat: Add mongoose models for question schema

---
+++ server/models/questionModel.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+const QuestionSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  options: {
+    type: [String],
+    required: true
+  },
+  correctAnswer: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+
+const QuizSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  questions: {
+    type: [QuestionSchema],
+    required: true
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('Quiz', QuizSchema);
+export const QuestionModel = mongoose.model("QuestionModel", QuestionSchema)
