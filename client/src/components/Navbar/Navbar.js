02affcca41b74fe1b68d6c870b3475a35326dd95
mdtalha109 <mdtalha1041@gmail.com>
2023-07-26 17:36:12 +0530

    feat: added navbar components with authentication components

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import { Action } from '@remix-run/router';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import './Navbar.css'
+import { logout } from '../../redux/user_reducer';
+
+const Navbar = () => {
+  const { isAuthenticated } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+
+  const navigateToLogin = (to) => {
+    navigate('/auth?action=login')
+  }
+
+  const navigateToRegister = (to) => {
+    navigate('/auth?action=register')
+  }
+
+  
+
+  const handleLogout = () => {
+    
+    dispatch(logout())
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link className="btn" to={'/'}>QuizLy</Link>
+      </div>
+
+      <div className='navbar_action_container'>
+      {isAuthenticated ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (<>
+             <button onClick={navigateToLogin}>  Login</button>
+             <button onClick={navigateToRegister}>Register</button></>
+            
+      )}</div>
+    </nav>
+  );
+};
+
+export default Navbar;
