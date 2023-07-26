ac7fb4fed9eae98c815f5c747dac8a4d40e15a8e
mdtalha109 <mdtalha1041@gmail.com>
2023-07-26 19:44:24 +0530

    feat: added question component

---
+++ client/src/components/Question/Question.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useFetchQuestion } from '../../hooks/FetchQuestion'
+import data from '../../data/data'
+
+
+
+
+const Questions = ({onChecked}) => {
+
+    const [checked, setChecked] = useState(undefined)
+    const [{isLoading, apiData, serverError}] = useFetchQuestion()
+
+    const {questions} = useSelector(state => state)
+    let trace = questions.trace
+    const question = questions.queue[trace]
+
+    useEffect(()=> {
+        
+    }, [])
+
+    const onSelect = (index) => {
+        setChecked(true)
+         //console.log("Selected: ", index)
+         onChecked(index)
+    }
+
+    if(isLoading) return <h3>Loading...</h3>
+
+    return (
+        <>
+          <div className="questions">
+            <h2 className="text-light">{trace+1} {question && question.question}</h2>
+            <ul key={question && question.id}>
+             
+              {question && question.options.map((option, index) => (
+                <li key={index}>
+                  <input
+                    type="radio"
+                    value={option}
+                    name="options"
+                    id={`q${index}-options`}
+                    onChange={()=> onSelect(index)}
+                  />
+                  <label className="text-primary" htmlFor={`q${index}-options`}>
+                    {option}
+                  </label>
+                  
+                  <div className="check"></div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      );
+      
+}
+
+export default Questions
