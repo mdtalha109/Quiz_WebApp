d4ff701335269c6fd814d42f669a1cae7dc09d7d
mdtalha109 <mdtalha1041@gmail.com>
2023-08-15 23:18:13 +0530

    basic setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './componens/App';
+import App from './pages/App';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
